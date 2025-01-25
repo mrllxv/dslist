8fadcf066ca5d0e8e24bf0f992e6e4de0eaf8175
Murillo Aragão <murilloaragaao@gmail.com>
2025-01-25 00:57:20 -0300

    Deploy ready

--- src/main/java/com/murilloaragao/dslist/projections/GameMinProjection.java
+++ src/main/java/com/murilloaragao/dslist/projections/GameMinProjection.java
@@ -4,7 +4,7 @@ package com.murilloaragao.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
